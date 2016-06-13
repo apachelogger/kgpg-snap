f9444e34bfb52341b13abb35bc797220c6899d5c
Harald Sitter <sitter@kde.org>
2016-06-13 10:59:10 +0200

    fix require maybe

--- rig.rb
+++ rig.rb
@@ -19,7 +19,7 @@
 # You should have received a copy of the GNU Lesser General Public
 # License along with this library.  If not, see <http://www.gnu.org/licenses/>.
 
-require '/var/lib/jenkins/tooling/ci-tooling/lib/apt'
+require '/var/lib/jenkins/ci-tooling/lib/apt'
 
 Apt.install('snapcraft')
 
